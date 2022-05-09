e1bad4f7edbc03080ffce1cd865823b063547876
hasan-sabuj <sabuj.hh@gmail.com>
2022-05-10 01:56:54 +0600

    Our 2nd commit

--- food_delivery_web_app/settings.py
+++ food_delivery_web_app/settings.py
@@ -60,6 +60,9 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
