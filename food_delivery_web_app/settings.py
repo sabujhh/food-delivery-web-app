232f1a0d48c52952e3ee92f59bda328ccd1fd533
hasan-sabuj <sabuj.hh@gmail.com>
2022-05-12 00:25:49 +0600

    next commit

--- food_delivery_web_app/settings.py
+++ food_delivery_web_app/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
